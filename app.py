7097801437f542c5c1abd94c7ce5d350a95e0a76
kanhaiyaraikavara39-pixel <kanhaiyaraikavara39@gmail.com>
2026-09-18 17:16:30 +0530

    Update ReleaseVersion from OB54 to OB55

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def make_request(encrypt, region, token):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB54"
+            "ReleaseVersion": "OB55"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         binary = response.content
