468840fe2c4d4dc9b967e8f8fb86e465c6a2963b
kanhaiyaraikavara39-pixel <kanhaiyaraikavara39@gmail.com>
2026-09-19 12:37:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ VALID_API_KEYS = {
 }
 
 # 🔢 Like limit tracking
-daily_limit = 20
+daily_limit = 200
 used_count = 0
 
 
@@ -77,7 +77,7 @@ async def send_request(encrypted_uid, token, url):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB54"
+            "ReleaseVersion": "OB55"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
@@ -134,7 +134,7 @@ def make_request(encrypt, region, token):
         elif region in {"BR", "US", "SAC", "NA"}:
             url = "https://client.us.freefiremobile.com/GetPlayerPersonalShow"
         else:
-            url = "https://clientbp.ggpolarbear.com/GetPlayerPersonalShow"
+            url = "https://clientbp.ppmainecoonghj.com/GetPlayerPersonalShow"
         edata = bytes.fromhex(encrypt)
         headers = {
             "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
@@ -203,7 +203,7 @@ def handle_requests():
             elif region in {"BR", "US", "SAC", "NA"}:
                 url = "https://client.us.freefiremobile.com/LikeProfile"
             else:
-                url = "https://clientbp.ggpolarbear.com/LikeProfile"
+                url = "https://clientbp.ppmainecoonghj.com/LikeProfile"
 
             asyncio.run(send_multiple_requests(uid, region, url))
 
